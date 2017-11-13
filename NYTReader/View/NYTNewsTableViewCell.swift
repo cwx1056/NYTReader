1831597d5dec6dea593cf84972b32a6b4cccba47
cwx1056 <wenx.chen.05@gmail.com>
2017-11-13 16:08:51 -0500

    Add all views to NewsTableViewCell

--- NYTReader/View/NYTNewsTableViewCell.swift
+++ NYTReader/View/NYTNewsTableViewCell.swift
@@ -10,20 +10,63 @@ import UIKit
 import SnapKit
 
 class NYTNewsTableViewCell: UITableViewCell {
-    
-    lazy fileprivate var photoView: NYTNewsPhotoView = {
-        return NYTNewsPhotoView(image: nil)
+
+    lazy fileprivate var photoView: UIImageView = {
+        return UIImageView()
     }()
     
-    lazy fileprivate var newsContentView: NYTNewsContentView = {
-        return NYTNewsContentView()
+    lazy fileprivate var photoAuthorLabel: UILabel = {
+        let newLabel = UILabel()
+        newLabel.numberOfLines = 0
+        newLabel.lineBreakMode = .byWordWrapping
+        newLabel.font = UIFont(name: "Avenir-Light", size: 10)
+        return newLabel
+    }()
+    
+    lazy fileprivate var titleLabel: UILabel = {
+        let newLabel = UILabel()
+        newLabel.numberOfLines = 0
+        newLabel.lineBreakMode = .byWordWrapping
+        newLabel.font = UIFont(name: "Avenir-Heavy", size: 16)
+        return newLabel
+    }()
+    
+    lazy fileprivate var contentLabel: UILabel = {
+        let newLabel = UILabel()
+        newLabel.numberOfLines = 0
+        newLabel.lineBreakMode = .byWordWrapping
+        newLabel.font = UIFont(name: "Avenir", size: 14)
+        return newLabel
+    }()
+    
+    lazy fileprivate var dateLabel: UILabel = {
+        let newLabel = UILabel()
+        newLabel.numberOfLines = 0
+        newLabel.lineBreakMode = .byWordWrapping
+        newLabel.font = UIFont(name: "Avenir-Light", size: 12)
+        newLabel.textColor = UIColor.lightGray.withAlphaComponent(0.75)
+        return newLabel
+    }()
+    
+    lazy fileprivate var authorLabel: UILabel = {
+        let newLabel = UILabel()
+        newLabel.numberOfLines = 0
+        newLabel.lineBreakMode = .byWordWrapping
+        newLabel.textAlignment = .right
+        newLabel.font = UIFont(name: "Avenir-Light", size: 8)
+        newLabel.textColor = UIColor.black.withAlphaComponent(0.8)
+        return newLabel
     }()
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
         contentView.addSubview(photoView)
-        contentView.addSubview(newsContentView)
+        contentView.addSubview(photoAuthorLabel)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(contentLabel)
+        contentView.addSubview(dateLabel)
+        contentView.addSubview(authorLabel)
         
         layoutInnerViews()
     }
@@ -33,18 +76,61 @@ class NYTNewsTableViewCell: UITableViewCell {
     }
     
     func setup(with news: News) {
-        photoView.setup(with: news.photo)
-        newsContentView.setup(with: news)
+        if let photo = news.photo {
+            authorLabel.text = photo.copyright
+            photoView.kf.setImage(with: URL(string: photo.url))
+            updatePhotoViewLayout(CGFloat(photo.height / photo.width))
+        } else {
+            authorLabel.text = nil
+            photoView.image = nil
+            updatePhotoViewLayout(0)
+        }
+        
+        authorLabel.text = news.byline
+        titleLabel.text = news.title
+        contentLabel.text = news.abstract
+        dateLabel.text = news.date.toNewsDateString()
     }
     
     fileprivate func layoutInnerViews() {
         photoView.snp.makeConstraints { make in
-            make.top.leading.trailing.equalTo(self.contentView)
+            make.leading.trailing.top.equalTo(self.contentView)
         }
         
-        newsContentView.snp.makeConstraints { make in
-            make.leading.trailing.bottom.equalTo(self.contentView)
-            make.top.equalTo(self.photoView.snp.bottom)
+        photoAuthorLabel.snp.makeConstraints { make in
+            make.leading.equalTo(self.contentView).offset(15)
+            make.trailing.equalTo(self.contentView).offset(-15)
+            make.top.equalTo(self.photoView.snp.bottom).offset(3)
+        }
+        
+        authorLabel.snp.makeConstraints { make in
+            make.leading.equalTo(self.contentView).offset(25)
+            make.trailing.equalTo(self.contentView).offset(-25)
+            make.top.equalTo(self.photoAuthorLabel.snp.bottom).offset(5)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.leading.equalTo(self.contentView).offset(25)
+            make.trailing.equalTo(self.contentView).offset(-25)
+            make.top.equalTo(self.authorLabel.snp.bottom).offset(3)
+        }
+        
+        contentLabel.snp.makeConstraints { make in
+            make.leading.trailing.equalTo(self.titleLabel)
+            make.top.equalTo(self.titleLabel.snp.bottom).offset(8)
+        }
+        
+        dateLabel.snp.makeConstraints { make in
+            make.leading.equalTo(self.contentLabel)
+            make.top.equalTo(self.contentLabel.snp.bottom).offset(10)
+            make.bottom.equalTo(self.contentView).offset(-15)
+        }
+    }
+    
+    fileprivate func updatePhotoViewLayout(_ ratio: CGFloat) {
+        photoView.snp.remakeConstraints { make in
+            make.leading.trailing.top.equalTo(self.contentView)
+            make.height.equalTo(self.photoView.snp.width).multipliedBy(ratio)
         }
     }
 
